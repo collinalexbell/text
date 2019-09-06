0a671fcc59183bafab59bb450c9a50e8e79bc2e8
slightlycyborg <collinalexbell@gmail.com>
2019-09-06 06:22:17 +0000

    ex command mode added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <unistd.h>
+
 #include <ncurses.h>
 
 using namespace std;
@@ -16,6 +18,14 @@ struct Buffer{
     }
 };
 
+enum Command {QUIT, UNKNOWN};
+
+void display(Buffer &b){
+    clear();
+    addstr(b.contents.c_str());
+	refresh();
+}
+
 char* getFileToOpen(int argc, char** argv){
     if (argc >= 2){
         return argv[1];
@@ -25,15 +35,64 @@ char* getFileToOpen(int argc, char** argv){
     exit(1);
 }
 
+Command ex_command_mode(){
+    int row, col;
+    getmaxyx(stdscr, row, col);
+    chtype last_line[col+1];
+
+    row = row-1;
+    mvinchstr(row, 0, last_line);
+    clrtoeol();
+
+    mvaddch(row, 0, ':');
+    refresh();
+
+    string cmd;
+    int cmd_i=0;
+
+    char c;
+
+    while(cmd_i<20 && c != 10){
+        c = getch();
+        addch(c);
+        refresh();
+        if(c != 10) cmd+=c;
+        cmd_i++;
+
+    }
+
+    mvaddchstr(row, 0, last_line);
+    refresh();
+    if(cmd == "q"){
+        return QUIT;
+    }
+    return UNKNOWN;
+}
+
+bool handle_commands(Buffer b){
+     char ch = getch();
+     if(ch == ':'){
+         Command cmd = ex_command_mode();
+         if(cmd == QUIT){
+             return true;
+         }
+     }
+
+     return false;
+}
+
 int main(int argc, char** argv){
     char* fname = getFileToOpen(argc, argv);
     Buffer buffer = Buffer(fname);
-
     initscr();			/* Start curses mode 		  */
-    addstr(buffer.contents.c_str());
+    noecho();
 
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
+    bool quit = false;
+
+    do{
+        display(buffer);
+        quit = handle_commands(buffer);
+    } while(!quit);
 	endwin();			/* End curses mode		  */
 
 	return 0;

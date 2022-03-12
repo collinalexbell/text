1bdaff5fb132fd314e4eb6ce97778f475f140e7f
kuberlog <collinalexbell@gmail.com>
2022-03-12 17:12:19 +0000

    use the same contents in test sections

--- src/InputProcessorTest.cpp
+++ src/InputProcessorTest.cpp
@@ -95,8 +95,8 @@ TEST_CASE("'x' should delete the character the cursor is on") {
 }
 
 TEST_CASE("'f' should find matching next character in line"){
+  string contents = "asdf\n";
   SECTION("character exists"){
-    string contents = "asdf\n";
     Buffer b = Buffer(contents);
     char *in = (char*)malloc(2);
     strcpy(in, "fd");
@@ -106,10 +106,9 @@ TEST_CASE("'f' should find matching next character in line"){
     REQUIRE(b.cursorX == 2);
   }
   SECTION("character doesn't exist"){
-    string contents = "asff\n";
     Buffer b = Buffer(contents);
     char *in = (char*)malloc(2);
-    strcpy(in, "fd");
+    strcpy(in, "fg");
     TestingInterface *interface = new TestingInterface(in);
     InputProcessor inputProcessor(interface);
     inputProcessor.handle_commands(b);
